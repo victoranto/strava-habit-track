675862fb50541af7a2e78fe1b26e30457fef982c
Víctor González <victor.gonzalez@wiris.com>
2023-12-07 20:17:29 +0100

    feat: create github table

--- src/components/Honeycomb/Honeycomb.js
+++ src/components/Honeycomb/Honeycomb.js
@@ -1,40 +1,38 @@
 import { useContext, useEffect } from 'react'
 import './Honeycomb.scss'
 import { UserContext } from '../../context/user'
-import { getActivities } from '../../services/strava';
+import { getActivities } from '../../services/strava'
+import { Card, Spinner } from 'react-bootstrap'
 
 export const Honeycomb = () => {
     const {state: userState, dispatch } = useContext(UserContext)
 
     function generarDiasDelAnio() {
-        const diasDelAnio = [];
-        const meses = 12; // Número de meses en un año
+        const diasDelAnio = []
+        const meses = 12
         
         for (let mes = 0; mes < meses; mes++) {
-            const diasEnElMes = new Date(2023, mes + 1, 0).getDate(); // Obtiene la cantidad de días en el mes actual
+            const diasEnElMes = new Date(2023, mes + 1, 0).getDate() 
             for (let dia = 1; dia <= diasEnElMes; dia++) {
-            const fecha = new Date(2023, mes, dia);
-            diasDelAnio.push(fecha.toLocaleDateString()); // Agrega la fecha al array como string
+            const fecha = new Date(2023, mes, dia)
+            diasDelAnio.push(fecha.toLocaleDateString())
             }
         }
-        
-        return diasDelAnio;
+        return diasDelAnio
     }
-    const listadoDiasAnio = generarDiasDelAnio();
 
-    function buscarFechaEnObjetos(fechaBuscada) {
-        const fechaEncontrada = userState.activities.find(objeto => {
-            const startD = new Date(objeto.start_date).toLocaleDateString();
-            return startD === fechaBuscada
-        });
-        return fechaEncontrada !== undefined ? true : false;
+    const  searchDateOnActivities = date => {
+        const is_date = userState.activities.find(activity => {
+            const startD = new Date(activity.start_date).toLocaleDateString()
+            return startD === date
+        })
+        return is_date !== undefined ? true : false
     }
-    // const resultadoBusqueda = buscarFechaEnObjetos("5/15/2023");
-    
 
     useEffect(() => {
         const getActs = async () => {
-            const act = await getActivities(userState.stravaToken, 1)
+            dispatch({type:'SET_STRAVA_ACTIVITIES_LOADING'})
+            const act = await getActivities(userState.stravaToken, 1, 2023)
             dispatch({type:'SET_STRAVA_ACTIVITIES', payload: act})
         }
         if(!userState.activities){
@@ -43,15 +41,25 @@ export const Honeycomb = () => {
     },[])
 
     return (
-        <section className="honeycomb">
-            <h3>Year activities 2023</h3>
-            {userState.activities && ( 
-                <div className="grid">
-                    {listadoDiasAnio.map( date  => {
-                        return (<div className={`item ${buscarFechaEnObjetos(date) ? "active" : ""}`} data-date={date} ></div>)
-                    })}
-                </div>
-            )}
+        <section className="honeycomb mt-3">
+            <Card bg="dark" data-bs-theme="dark">
+                <Card.Header>Year activities 2023</Card.Header>
+                <Card.Body>
+                    {userState.activitiesLoading && (
+                        <>
+                            <Spinner animation="border" role="status"></Spinner>{' '}
+                            <span><small>Loading...</small></span>
+                        </>
+                    )}
+                    {userState.activities && ( 
+                        <div className="grid">
+                            {generarDiasDelAnio().map( date  => {
+                                return (<div className={`item ${searchDateOnActivities(date) ? "active" : ""}`} data-date={date} key={date} ></div>)
+                            })}
+                        </div>
+                    )}
+                </Card.Body>
+            </Card>
         </section>
     )
 }
